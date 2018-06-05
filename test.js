b5b8d050d68c96042aa4905feef5f6fe17aef633
rupeshiya <rupeshiya@gmail.com>
2018-06-06 01:25:22 +0530

    test

---
+++ test.js
@@ -0,0 +1,29 @@
+console.log('Hello world ,here we go !!');
+console.log('This is the list ofimportant npm packages that you should know');
+console.log(`
+1.Lodash
+2.jdalton
+3.request
+4.simov
+5.chalk
+6.sindresorhus
+7.express
+8.dougwilson
+9.react
+10.acdlite
+11.async
+12.aerly
+13.commander
+14.abetomo
+15.bluebird
+16.debug
+17.underscore
+18.moment
+19.fs
+20.body-parser
+21.Cheerio
+  `);
+  console.log('Know about the given above stuff to become a good developer of node.js');
+  console.log('Feel free to suggest the change in list');
+  console.log('Suggest changes by shooting a issue at github');
+console.log('made by rupeshiya');
